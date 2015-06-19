065c2ce1e99bdf86f330f3684263080d047dcd12
Shivraj <shivraj@Himanshus-Mac-mini.local>
2015-06-19 17:00:02 +0530

    first commit

--- Guess/ViewController.swift
+++ Guess/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func checkIfCountIsCorrect(sender: UIButton) {
+        var random = Int(arc4random_uniform(10))
+        println(random)
+        if random == sender.currentTitle!.toInt() {
+            result.text = "Eureka!!"
+            result.textColor = UIColor.greenColor()
+        } else {
+            result.text = "Better luck next time"
+            result.textColor = UIColor.redColor()
+        }
+    }
 
 }
 
